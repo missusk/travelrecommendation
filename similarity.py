a739376610c118bf99681e33dfc1a6590526646b
Maria Khan <82167463+missusk@users.noreply.github.com>
2024-07-07 12:28:50 +0530

    Delete similarity.py

--- similarity.py
+++
@@ -1,6 +0,0 @@
-from sklearn.metrics.pairwise import cosine_similarity 
-import streamlit as st
-
-@st.cache_resource
-def calculate_cosine_similarity(_tfidf_matrix):
-    return cosine_similarity(_tfidf_matrix)
